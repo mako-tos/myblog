6b7fcd16ccb6382db4c8033a8cf1d3724ef36e5a
mako-tos <makotos98@gmail.com>
2020-05-27 12:49:48 +0900

    add twitter timeline to footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitterSquare, faGithub } from '@fortawesome/free-brands-svg-icons';
 import config from '../../config/site';
+import LazyLoad from 'react-lazyload';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 const Wrapper = styled.footer`
   position: relative;
@@ -30,6 +32,28 @@ const Text = styled.div`
   }
 `;
 
+const TwitterPlaceholderWrapper = styled.div`
+  color: ${props => props.theme.colors.white.light};
+  a {
+    margin-left: 0.5em;
+    color: ${props => props.theme.colors.white.light};
+  }
+`;
+
+const TwitterPlaceholder = () => (
+  <TwitterPlaceholderWrapper>
+    <FontAwesomeIcon icon={faTwitterSquare} />
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href={`https://twitter.com/${config.twitter}`}
+      itemProp="url"
+    >
+      {config.twitter}
+    </a>
+  </TwitterPlaceholderWrapper>
+);
+
 const Footer = () => (
   <Wrapper
     role="contentinfo"
@@ -47,17 +71,6 @@ const Footer = () => (
       </span>
     </Text>
     <Text>
-      <FontAwesomeIcon icon={faTwitterSquare} />
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href={`https://twitter.com/${config.twitter}`}
-        itemProp="url"
-      >
-        {config.twitter}
-      </a>
-    </Text>
-    <Text>
       <FontAwesomeIcon icon={faGithub} />
       <a
         target="_blank"
@@ -67,6 +80,15 @@ const Footer = () => (
       >
         {config.github}
       </a>
+    </Text>
+    <Text>
+      <LazyLoad once placeholder={<TwitterPlaceholder />}>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName={config.twitter}
+          options={{ height: 400, lang: 'ja' }}
+        />
+      </LazyLoad>
     </Text>
   </Wrapper>
 );

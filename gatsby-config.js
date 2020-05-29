15304ae02db5b2aaa0e76a3b9f20abe724ea87dc
mako-tos <makotos98@gmail.com>
2020-05-29 14:20:53 +0900

    remove duplicated condition

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-source-microcms',
